8c853bde6a9a49f44a660cac29ec5875fa974b6d
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-28 20:19:45 +0530

    complte btn clicked and count number

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("add this file");
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+
+const all = document.querySelectorAll(".love-icon");
+const navLove = getElement("nav-love");
+let count = 0;
+
+for (const allArray of all) {
+  allArray.addEventListener("click", function () {
+    count++;
+    navLove.innerText = count;
+  });
+}
